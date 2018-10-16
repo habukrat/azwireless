e6289792e01e9b9e603d75acb9b8ac45e384f736
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-10-15 18:14:17 -0700

    added map

---
+++ main.js
@@ -0,0 +1,19 @@
+
+$('#sendMessage').click(function(){
+	$('#sendMessage').hide();
+	$('#thankyou').show();
+});
+
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(34.173310, -118.554197);
+  var mapOptions = {
+    center: myCenter,
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({position: myCenter});
+  marker.setMap(map);
+}
+
