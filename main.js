385cad529947082bf837f5ffaf483f9a3909adc4
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-10-17 19:02:40 -0700

    working on home page

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ function myMap() {
   marker.setMap(map);
 }
 
+var slideIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
